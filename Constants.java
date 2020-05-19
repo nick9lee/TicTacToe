07a9f98ae8f9edd29c9e839076dc92d1d9b54ffa
Nick Lee <60014503+nick9lee@users.noreply.github.com>
2020-05-18 22:38:11 -0600

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,8 +1,4 @@
-/**
- * contains constant variables that represent the game board possible values.
- * @author university of calgary
- *
- */
+
 public interface Constants {
 	static final char SPACE_CHAR = ' ';
 	static final char LETTER_O = 'O';
